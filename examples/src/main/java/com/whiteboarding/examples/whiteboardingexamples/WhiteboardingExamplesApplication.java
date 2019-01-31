1c67bf84644cb536b93846237da8b169820054f9
joja5627 <joja5627@gmail.com>
2019-01-31 04:52:50 -0800

    gitignore and spring initilizer project

---
+++ examples/src/main/java/com/whiteboarding/examples/whiteboardingexamples/WhiteboardingExamplesApplication.java
@@ -0,0 +1,14 @@
+package com.whiteboarding.examples.whiteboardingexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteboardingExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteboardingExamplesApplication.class, args);
+	}
+
+}
+
